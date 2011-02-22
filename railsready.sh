094bbf293db982dcd5538f9a8de69e4ba83d9f1f
Josh Frye <joshfng@gmail.com>
2011-02-22 14:31:02 -0500

    Bump patchlevel due to recent Ruby security release.

--- railsready.sh
+++ railsready.sh
@@ -12,10 +12,10 @@ shopt -s nocaseglob
 set -e
 
 ruby_version="1.9.2"
-ruby_version_string="1.9.2p136"
-ruby_source_url="ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p136.tar.gz"
-ruby_source_tar_name="ruby-1.9.2-p136.tar.gz"
-ruby_source_dir_name="ruby-1.9.2-p136"
+ruby_version_string="1.9.2p180"
+ruby_source_url="ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p180.tar.gz"
+ruby_source_tar_name="ruby-1.9.2-p180.tar.gz"
+ruby_source_dir_name="ruby-1.9.2-p180"
 script_runner=$(whoami)
 railsready_path=$(cd && pwd)/railsready
 log_file="$railsready_path/install.log"
